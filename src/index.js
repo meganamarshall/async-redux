c269d4d7844052f11e8b1ba45d3b2e66bf003676
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-29 14:16:56 -0700

    adding logger

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import {
   applyMiddleware
 } from 'redux';
 
-// const logger = store => next => action
+const logger = store => next => action => {
+  console.log('before action', store.getState());
+  next(action);
+  console.log('after action', store.getState());
+};
 
 
 function reducer(state = {}, action) {
   switch(action.type) {
     case 'WHAT':
-      return 'whatever';
+      return 'whatever this is the new state';
     default:
       return state;
   }
@@ -17,5 +21,9 @@ function reducer(state = {}, action) {
 
 const store = createStore(
   reducer,
-  applyMiddleware()
-)
+  applyMiddleware(logger)
+);
+
+store.dispatch({
+  type: 'WHAT'
+});
