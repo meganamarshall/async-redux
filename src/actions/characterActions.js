14c5ea0e7e67ddb3013262ec2f819ced22873642
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-30 13:56:46 -0700

    finished promises refactor

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -1,27 +1,9 @@
+import { createAction } from 'promise-middleware-redux';
 import { getCharacters } from '../services/airbenderApi';
 
-export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
-export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
-export const FETCH_CHARACTERS_ERROR = 'FETCH_CHARACTERS_ERROR';
-
-
-export const fetchCharacters = () => dispatch => {
-  dispatch({
-    type: FETCH_CHARACTERS_LOADING
-  });
-
-  return getCharacters() 
-    .then(character => {
-      console.log(character);
-      dispatch({
-        type: FETCH_CHARACTERS,
-        payload: character
-      });
-    })
-    .catch(err => {
-      dispatch({
-        type: FETCH_CHARACTERS_ERROR,
-        payload: err
-      });
-    });
-};
+export const [
+  fetchCharacters,
+  FETCH_CHARACTERS,
+  FETCH_CHARACTERS_LOADING,
+  FETCH_CHARACTERS_ERROR
+] = createAction('FETCH_CHARACTERS', getCharacters);
