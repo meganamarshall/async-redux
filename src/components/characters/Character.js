166971ecd07078bbbad13f27d22b2636a048029f
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-30 09:27:03 -0700

    working on Character and Api service

---
+++ src/components/characters/Character.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Character({ character }) {
+  return (
+    <section>
+      <img src=""/>
+      <p></p>
+    </section>
+  );
+}
+
+Character.propTypes = {
+
+}
+
+export default Character;
---
+++ src/services/airbenderApi.js
@@ -0,0 +1,16 @@
+export default getCharacters(url) {
+  return fetch('https://last-airbender-api.herokuapp.com/api/v1/characters')
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'Unable to fetch Characters';
+
+      return json;
+    })
+    .then(json => {
+      json.map(character => ({
+        id: character._id,
+        name: character.name,
+        image: character.photoUrl
+      }))
+    })
+}
